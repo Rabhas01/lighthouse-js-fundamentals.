aab3a2f8d3c3521cf4a24c1513a02178a0781621
Rabhas <rabhas.vashisht@gmail.com>
2022-02-16 21:16:29 +0000

    sayHello function

---
+++ even.js
@@ -0,0 +1,5 @@
+let isEven = function (x){
+  return x %2 === 0
+}
+console.log(isEven(10));
+console.log(isEven(5));
---
+++ hello.js
@@ -0,0 +1,5 @@
+const returnSayHello  = function (x) {
+  return "Hello, " + x;
+}
+const greeting = returnSayHello('Rabhas');
+console.log(greeting);
