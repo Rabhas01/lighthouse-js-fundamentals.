0e3c4c5d7264d8f1ce970ee780f076176a422606
Rabhas <rabhas.vashisht@gmail.com>
2022-02-17 22:17:32 +0000

    Chakkenge

---
+++ ingredients.js
@@ -0,0 +1,13 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+  let i = 0;
+  while(i < ingredients.length ){
+  console.log(ingredients[i]); i++;
+}
+let x = 0;
+  for(x===0; x < ingredients.length; x++){
+  console.log(ingredients[x]);
+}
+
+for (var z = ingredients.length -1; z >=0; z--) {
+  console.log(ingredients[z]);
+}
